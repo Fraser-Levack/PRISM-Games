bd5318713882012ef671859e8c3fb182774d8648
Fraser Levack <flevack28@gmail.com>
2026-02-17 16:40:16 +0000

    chore: sort of fixing credits

--- src/pages/Credits.tsx
+++ src/pages/Credits.tsx
@@ -1,30 +1,41 @@
 import { Link } from 'react-router-dom';
+import '../styles/Credits.css';
 
 const Credits = () => {
   return (
     <div className="credits">
       <h1>Credits</h1>
       <div className="credits-content">
-        <h2>Development Team</h2>
+        <h2>Development </h2>
         <ul>
-          <li>Fraser Levack - Lead Developer</li>
+          <li><strong>Fraser Levack</strong> - Lead Developer</li>
         </ul>
+
+        <p> Many thanks to <strong>Professor Gethin Norman</strong> 
+          <br></br>for his continued support and guidance 
+          <br></br>throughout the development of this project.
+        </p>
         
         <h2>Technologies Used</h2>
         <ul>
           <li>React</li>
           <li>TypeScript</li>
           <li>Vite</li>
-          <li>React Router</li>
           <li>Three.js</li>
           <li>PRISM Model Checker</li>
         </ul>
       </div>
+
+      <div className="bottom-links">
+      <a href="https://www.prismmodelchecker.org/" target="_blank" rel="noopener noreferrer" className="nav-link">
+        Visit PRISM Model Checker
+      </a>
       
       <div className="navigation">
         <Link to="/" className="nav-link">
           Back to Home
         </Link>
+      </div>
       </div>
     </div>
   );
